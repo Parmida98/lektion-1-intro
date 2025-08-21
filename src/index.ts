6fe335d625187ea959c546605a8d7db34dc5e393
Parmida98 <Parmida.ahmadi@stud.sti.se>
2025-08-21 12:58:22 +0200

    add: database and security

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,23 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}`)
     console.log(secret)
 })
+
+
+import { closeDB, runDB } from "./db/database.js"
+async function startServer() {
+    try {
+        await runDB()
+        app.listen(port, () => {
+            console.log(`Listening to port ${port}`)
+            console.log(`Start the app: http://localhost:${port}`)
+        })
+        process.on("SIGINT", async () => {
+            console.log("Cleaning up...")
+            await closeDB()
+            process.exit(0)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+startServer()
